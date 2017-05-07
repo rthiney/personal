579790b6c8e093d60a44a9ccde3d94b76ea1cddc
Raphael Thiney <raphael.thiney@gmail.com>
2017-05-07 19:18:30 -0400

    Update environment.prod.ts
    removed key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 
 export const environment = {
   production: false,
-  insightKey: '81ad00b1-5009-482d-82e6-2664258ca29a',
-  sendgrid: {
-  key : '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',
-   billingTemplate : '661d5e3f-113c-4ee1-8b93-76b5ac74523b'
-  }
+  insightKey: '81ad00b1-5009-482d-82e6-2664258ca29a' 
 };
