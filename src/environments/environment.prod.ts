0d64e017fe0f2e5e569a17ded3eeacc8c0cf7b62
Raphael Thiney <raphael.thiney@gmail.com>
2017-05-09 00:07:08 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,5 @@
 export const environment = {
   production: true,
     envName: 'Production',
-  insightKey: '81ad00b1-5009-482d-82e6-2664258ca29a',
-  sendgrid: {
-   key : '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',
-   billingTemplate : '661d5e3f-113c-4ee1-8b93-76b5ac74523b'
-  }
+  insightKey: '81ad00b1-5009-482d-82e6-2664258ca29a'
 };
