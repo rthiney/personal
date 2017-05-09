6a824f3d9bea48280f0883d592c3846be4a96712
Raphael Thiney <raphael.thiney@gmail.com>
2017-05-09 00:12:16 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,10 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+
 export const environment = {
   production: false,
-  envName:'Development',
+    envName: 'dev',
   insightKey: '81ad00b1-5009-482d-82e6-2664258ca29a',
-  sendgrid: {
-   key : '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',
-   billingTemplate : '661d5e3f-113c-4ee1-8b93-76b5ac74523b'
-  }
+    sendgridkey : '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'
 };
