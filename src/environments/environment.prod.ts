4abbf6598056eb930baee0b24eb2b3ec64bc93e9
Raphael Thiney <raphael.thiney@gmail.com>
2017-05-09 00:08:30 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@
 export const environment = {
   production: true,
     envName: 'Production',
-  insightKey: '81ad00b1-5009-482d-82e6-2664258ca29a'
+  insightKey: '81ad00b1-5009-482d-82e6-2664258ca29a',
+    sendgridkey : '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'
 };
